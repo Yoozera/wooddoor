96e3c720d3c9fb3839b168592da996decfa86e1f
shayan <shayanjamalishjtkd@gmail.com>
2026-08-20 07:35:53 -0800

    switch to postgresql database

--- wooddoor/settings.py
+++ wooddoor/settings.py
@@ -2,6 +2,7 @@
 Django settings for wooddoor project.
 """
 import os
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -71,11 +72,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'wooddoor.wsgi.application'
 
 # Database
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
